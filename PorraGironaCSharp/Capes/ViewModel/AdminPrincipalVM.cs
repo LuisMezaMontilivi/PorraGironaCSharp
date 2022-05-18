1786bb6ad789eb85746d0b9e7484036b6abd2e19
Luis Meza <a2106917@institutmontilivi.cat>
2022-05-18 09:14:15 +0200

    Afegir parts que tenía ja creades en un projecte a part i comprovar el seu funcionament

---
+++ PorraGironaCSharp/Capes/ViewModel/AdminPrincipalVM.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PorraGironaCSharp.Core;
+
+namespace PorraGironaCSharp.Capes.ViewModel
+{
+    class AdminPrincipalVM : ObservableObject
+    {
+        public RelayCommand UltimPartitComanda { get; set; }
+
+        public UltimPartitViewModel UltimVm { get; set; }
+
+
+        private object vistaActual;
+
+        public object CurrentView
+        {
+            get { return vistaActual; }
+            set
+            {
+                vistaActual = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public AdminPrincipalVM()
+        {
+            UltimVm = new UltimPartitViewModel();
+            CurrentView = UltimVm;
+            UltimPartitComanda = new RelayCommand(o => { CurrentView = UltimVm; });
+        }
+    }
+}
