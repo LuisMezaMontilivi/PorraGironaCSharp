c6bc7264aacbe85547318893a28f9eaf47ac7c16
Luis Meza <a2106917@institutmontilivi.cat>
2022-05-25 23:26:00 +0200

    Eliminación de basura

--- PorraGironaCSharp/Capes/ViewModel/UltimPartitViewModel.cs
+++ PorraGironaCSharp/Capes/ViewModel/UltimPartitViewModel.cs
@@ -7,35 +7,7 @@ using System.Threading.Tasks;
 
 namespace PorraGironaCSharp.Capes.ViewModel
 {
-    class UltimPartitViewModel : ObservableObject
+    class UltimPartitViewModel
     {
-        public RelayCommand AltaEquipComanda { get; set; }
-
-        public RelayCommand AltaUsuariComanda { get; set; }
-
-        public AltaEquipViewModel AltaEquipVm { get; set; }
-
-        public AltaUsuariViewModel AltaUsuariVm { get; set; }
-
-        private object vistaActual;
-
-        public object CurrentView
-        {
-            get { return vistaActual; }
-            set
-            {
-                vistaActual = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public UltimPartitViewModel()
-        {
-            AltaEquipVm = new AltaEquipViewModel();
-            AltaUsuariVm = new AltaUsuariViewModel();
-            CurrentView = AltaUsuariVm;
-            AltaUsuariComanda = new RelayCommand(o => { CurrentView = AltaUsuariVm; });
-            AltaEquipComanda = new RelayCommand(o => { CurrentView = AltaEquipVm; });
-        }
     }
 }
